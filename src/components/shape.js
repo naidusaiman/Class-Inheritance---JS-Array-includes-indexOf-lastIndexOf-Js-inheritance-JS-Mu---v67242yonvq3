864c639e67339c6a9b2b6c337a79ca895d493ddd
naidusaiman <79516689+naidusaiman@users.noreply.github.com>
2022-09-25 23:30:31 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,13 @@
 // declare class
+class Shape {
+  var color = red;
+  drawShape() {
+    console.log("Shape Drawn"); 
+  }
+  calculateArea() {
+    console.log("Shape Area Calculated"); 
+  }
+}
+module.exports = Circle;
 
 // export class using module.exports

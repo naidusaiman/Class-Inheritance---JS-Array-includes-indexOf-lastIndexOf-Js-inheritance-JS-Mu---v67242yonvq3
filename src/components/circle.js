38b550fbb4e0d16198a1cfd3ece9c5df128d8eac
naidusaiman <79516689+naidusaiman@users.noreply.github.com>
2022-09-25 23:30:00 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,12 @@
 // import using require
 
 // declare class
-
+import Shape require ('');
+class Circle extends Shape {
+ @override
+  calculateArea(){
+   console.log("Circle Area Drawn"); 
+  }
+}
 
 // export class using module.exports
